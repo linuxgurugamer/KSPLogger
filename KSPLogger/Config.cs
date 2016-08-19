365517919ce185192c958300a01d9159bbc95f50
Jonathan Bayer <linuxgurugamer@gmail.com>
2016-08-18 22:20:04 -0400

    Initial Release

---
+++ KSPLogger/Config.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using KSP.UI.Screens;
+using KSPAssets;
+
+namespace KSPLogger
+{
+    
+    public class Config
+    {
+        public string ROOT_PATH = KSPUtil.ApplicationRootPath;
+        string DIR = "GameData/KSPLogger";
+        static readonly string CFG_FILE = "KSPLogger.cfg";
+
+        static readonly string NODENAME = "KSPLogger";
+        private static ConfigNode configFile = null;
+        private static ConfigNode configFileNode = null;
+
+        // Data items follow
+        public string filePrefix = "dataLog";
+        public string fileSuffix = ".txt";
+        public bool unixFormat = false;
+        public string eol = "\r\n";
+        public bool onePerFile = true;
+        public string separator = ",";
+        public bool singleLine = true;
+        public int decimalPlaces = 0;
+
+        // From FlightGlobals
+
+        public bool ship_geeForce = false;
+        public bool ship_latitude = false;
+        public bool ship_longitude = false;
+        public bool ship_obtSpeed = false;
+        public bool ship_srfSpeed = false;
+        public bool ship_verticalSpeed = false;
+
+        //fromFlightGlobals.ActiveVessel
+        public bool altitude = false;
+        public bool atmDensity = false;
+        public bool atmosphericTemperature = false;
+        public bool currentStage = false;
+        public bool distanceToSun = false;
+        public bool geeForce = false;
+        public bool geeForce_immediate = false;
+        public bool heightFromSurface = false;
+        public bool heightFromTerrain = false;
+        public bool horizontalSrfSpeed = false;
+        public bool indicatedAirSpeed = false;
+        public bool landedAt = false;
+        public bool mach = false;
+        public bool missionTime = false;
+        public bool obt_speed = false;
+
+        public bool speed = false;
+        public bool vesselName = false;
+
+        void parseConfigNode(ref ConfigNode root)
+        {
+            try { filePrefix = root.GetValue("filePrefix"); } catch { }
+            try { fileSuffix = root.GetValue("fileSuffix"); } catch { }
+            
+            try { unixFormat = Boolean.Parse(root.GetValue("unixFormat")); } catch { }
+            try { onePerFile = Boolean.Parse(root.GetValue("onePerFile")); } catch { }
+
+            try { separator = root.GetValue("separator"); } catch { }
+
+            try { singleLine = Boolean.Parse(root.GetValue("singleLine")); } catch { }
+
+            try { singleLine = Boolean.Parse(root.GetValue("singleLine")); } catch { }
+            try { decimalPlaces = Convert.ToUInt16(root.GetValue("decimalPlaces")); } catch (Exception) { }
+            try { ship_geeForce = Boolean.Parse(root.GetValue("ship_geeForce")); } catch { }
+            try { ship_latitude = Boolean.Parse(root.GetValue("ship_latitude")); } catch { }
+            try { ship_longitude = Boolean.Parse(root.GetValue("ship_longitude")); } catch { }
+            try { ship_obtSpeed = Boolean.Parse(root.GetValue("ship_obtSpeed")); } catch { }
+            try { ship_srfSpeed = Boolean.Parse(root.GetValue("ship_srfSpeed")); } catch { }
+            try { ship_verticalSpeed = Boolean.Parse(root.GetValue("ship_verticalSpeed")); } catch { }
+
+            try { altitude = Boolean.Parse(root.GetValue("altitude")); } catch { }
+            try { atmDensity = Boolean.Parse(root.GetValue("atmDensity")); } catch { }
+            try { atmosphericTemperature = Boolean.Parse(root.GetValue("atmosphericTemperature")); } catch { }
+            try { currentStage = Boolean.Parse(root.GetValue("currentStage")); } catch { }
+            try { distanceToSun = Boolean.Parse(root.GetValue("distanceToSun")); } catch { }
+            try { geeForce = Boolean.Parse(root.GetValue("geeForce")); } catch { }
+            try { geeForce_immediate = Boolean.Parse(root.GetValue("geeForce_immediate")); } catch { }
+            try { heightFromTerrain = Boolean.Parse(root.GetValue("heightFromTerrain")); } catch { }
+            try { horizontalSrfSpeed = Boolean.Parse(root.GetValue("horizontalSrfSpeed")); } catch { }
+            try { indicatedAirSpeed = Boolean.Parse(root.GetValue("indicatedAirSpeed")); } catch { }
+            try { landedAt = Boolean.Parse(root.GetValue("landedAt")); } catch { }
+            try { mach = Boolean.Parse(root.GetValue("mach")); } catch { }
+            try { missionTime = Boolean.Parse(root.GetValue("missionTime")); } catch { }
+            try { obt_speed = Boolean.Parse(root.GetValue("obt_speed")); } catch { }
+            try { heightFromSurface = Boolean.Parse(root.GetValue("heightFromSurface")); } catch { }
+            try { vesselName = Boolean.Parse(root.GetValue("vesselName")); } catch { }
+        }
+
+
+        public void LoadConfiguration()
+        {
+            configFile = ConfigNode.Load(ROOT_PATH + DIR + "/"  +CFG_FILE);
+
+            if (configFile != null)
+            {
+                Log.Info("Loading config: " + ROOT_PATH + DIR + "/" + CFG_FILE);
+                configFileNode = configFile.GetNode(NODENAME);
+                if (configFileNode != null)
+                {
+                    parseConfigNode(ref configFileNode);
+                    if (unixFormat)
+                        eol = "\n";
+                }
+            }
+            else
+                Log.Info("No config found");
+        }
+
+
+    }
+}
---
+++ KSPLogger/KSPLogger.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+namespace KSPLogger
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class KSPLogger : MonoBehaviour
+    {
+        Config cfg = new Config();
+
+
+
+        string line = "";
+        string filenames = "";
+
+        public void WriteLine()
+        {
+            Log.Info("WriteLine, file: " + cfg.ROOT_PATH + "/" + cfg.filePrefix);
+            if (cfg.onePerFile == false)
+            {
+                filenames = cfg.ROOT_PATH + "/" + cfg.filePrefix + cfg.fileSuffix;
+                if (cfg.singleLine)
+                    File.WriteAllText(filenames, line + cfg.eol);
+                else
+                    File.AppendAllText(filenames, line + cfg.eol);
+                line = "";
+            }
+        }
+
+        public void WriteFile(string fileName, string value)
+        {
+            if (cfg.onePerFile == false)
+            { 
+                line += value + cfg.separator;
+            }
+            else
+            {
+                string fname = cfg.ROOT_PATH + cfg.filePrefix + "." + fileName + cfg.fileSuffix;
+                filenames += fname + ":";
+                Log.Info("WriteFile, file: " + cfg.ROOT_PATH + "/" + cfg.filePrefix);
+
+                if (cfg.singleLine)
+                    File.WriteAllText(fname, value + cfg.eol);
+                else
+                    File.AppendAllText(fname, value + cfg.eol);
+            }
+            //  Log.Debug(fmt);
+        }
+
+        public void OnDestroy()
+        {
+            if (cfg.singleLine)
+            {
+
+                char[] delimiterChars = { ':' };
+                string[] fname = filenames.Split(delimiterChars);
+                foreach (string s in fname)
+                {
+                    File.Delete(s);
+                }
+            }
+        }
+
+        public void Start()
+        {
+            cfg.LoadConfiguration();
+        }
+
+        public void LateUpdate()
+        {
+            filenames = "";
+            // From FlightGlobals
+            if (cfg.ship_geeForce)
+                WriteFile("ship_geeForce", FlightGlobals.ship_geeForce.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.ship_latitude)
+                WriteFile("ship_latitude", FlightGlobals.ship_latitude.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.ship_longitude)
+                WriteFile("ship_longitude", FlightGlobals.ship_longitude.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.ship_obtSpeed)
+                WriteFile("ship_obtSpeed", FlightGlobals.ship_obtSpeed.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.ship_srfSpeed)
+                WriteFile("ship_srfSpeed", FlightGlobals.ship_srfSpeed.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.ship_verticalSpeed)
+                WriteFile("ship_verticalSpeed", FlightGlobals.ship_verticalSpeed.ToString("F" + cfg.decimalPlaces.ToString()));
+
+            //fromFlightGlobals.ActiveVessel
+            if (cfg.altitude)
+                WriteFile("altitude", FlightGlobals.ActiveVessel.altitude.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.atmDensity)
+                WriteFile("atmDensity", FlightGlobals.ActiveVessel.atmDensity.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.atmosphericTemperature)
+                WriteFile("atmosphericTemperature", FlightGlobals.ActiveVessel.atmosphericTemperature.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.currentStage)
+                WriteFile("currentStage", FlightGlobals.ActiveVessel.currentStage.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.distanceToSun)
+                WriteFile("distanceToSun", FlightGlobals.ActiveVessel.distanceToSun.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.geeForce)
+                WriteFile("geeForce", FlightGlobals.ActiveVessel.geeForce.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.geeForce_immediate)
+                WriteFile("geeForce_immediate", FlightGlobals.ActiveVessel.geeForce_immediate.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.heightFromSurface)
+                WriteFile("heightFromSurface", FlightGlobals.ActiveVessel.heightFromSurface.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.heightFromTerrain)
+                WriteFile("heightFromTerrain", FlightGlobals.ActiveVessel.heightFromTerrain.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.horizontalSrfSpeed)
+                WriteFile("horizontalSrfSpeed", FlightGlobals.ActiveVessel.horizontalSrfSpeed.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.indicatedAirSpeed)
+                WriteFile("indicatedAirSpeed", FlightGlobals.ActiveVessel.indicatedAirSpeed.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.landedAt)
+                WriteFile("landedAt", FlightGlobals.ActiveVessel.landedAt);
+            if (cfg.mach)
+                WriteFile("mach", FlightGlobals.ActiveVessel.mach.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.missionTime)
+                WriteFile("missionTime", FlightGlobals.ActiveVessel.missionTime.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.obt_speed)
+                WriteFile("obt_speed", FlightGlobals.ActiveVessel.obt_speed.ToString("F" + cfg.decimalPlaces.ToString()));
+            if (cfg.vesselName)
+                WriteFile("vesselName", FlightGlobals.ActiveVessel.vesselName);
+
+
+        }
+
+    }
+}
